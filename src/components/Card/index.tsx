e12b24b05129f0b49095d7ebc27ab3c4bac5f157
LenyPython <buniadyserv@gmail.com>
2021-12-07 20:16:05 +0100

    Added svg from BavkButton and fuctioning Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,27 +1,33 @@
 import styled from 'styled-components'
+import {useAppDispatch} from '../../app/hooks'
+import {CountryInterface, setCountry} from '../../slices/Global'
 
 const Container = styled.div`
-  background: var(--main-elements-color);
+  display: flex;
+  flex-direction: column;
   width: 275px;
-  margin: 20px;
+  margin: 20px 0;
+  border-radius: 5px;
+  border: 5px solid hsla(0,0%,0%,.1);
+  overflow: hidden;
+  cursor: pointer;
+  &:hover {
+    border: 5px solid hsla(0,0%,0%,.6);
+  }
   img { width: 100%; }
   h3 { margin: 2em 0; }
   p { margin: .4em 0; }
-  div { margin: 2em; }
+  div { margin: auto 2em 2em; }
 `
-interface Props {
-  name: string
-  population: number
-  region: string
-  capital: string
-  src: string
-}
-const CountryCard: React.FC<Props> = ({name, src, population, region, capital}) => {
-  return <Container>
-    <img src={src} alt='coutry_flag' />
+const CountryCard: React.FC<CountryInterface> = (Props) => {
+  const dispatch = useAppDispatch()
+  const { region, population, capital, name, flag  } = Props
+  return <Container
+      onClick={()=>dispatch(setCountry(Props)) }>
+    <img src={flag} alt='coutry_flag' />
     <div>
       <h3>{name}</h3>
-      <p><b>Population:</b> {population}</p>
+      <p><b>Population:</b> {population.toLocaleString()}</p>
       <p><b>Region:</b> {region}</p>
       <p><b>Capital:</b> {capital}</p>
     </div>
